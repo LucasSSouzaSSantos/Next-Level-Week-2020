438b2c64f0969d87b975a58da98d618a42933c3a
Lucas <lucsousan@gmail.com>
2020-08-12 15:27:11 -0300

    começando hoje

--- src/server.js
+++ src/server.js
@@ -1,8 +1,14 @@
-require('express')()
+const express = require('express')
+const server = express()
+
+server.use(express.static("public"))
 .get("/", (req, res) => {
-  return res.send("Hi from NL")  
+  return res.sendFile(__dirname + "/views/index.html")  
 })
 .get("/study", (req, res) => {
-    return res.send("Página study")
+  return res.sendFile(__dirname + "/views/study.html")
 })
+  .get("/give-classes", (req, res) => {
+    return res.sendFile(__dirname + "/views/give-classes.html")
+  })
 .listen(5500)
