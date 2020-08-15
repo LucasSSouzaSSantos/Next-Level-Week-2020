d4e4955697fbf61b69f89894aa409eddf1060811
Lucas <lucsousan@gmail.com>
2020-08-15 12:20:09 -0300

    SALVANDO 1

--- src/database/db.js
+++ src/database/db.js
@@ -2,6 +2,28 @@ const Database = require('sqlite-async')
 Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db) {
-    console.log('cheguei aqui')
-    console.log(db)
+    db.exec(`
+        CREATE TABLE IF NOT EXISTS proffys (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            avatar TEXT,
+            whatsapp TEXT,
+            bio TEXT
+        );
+
+        CREATE TABLE IF NOT EXISTS classes(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            subject TEXT,
+            cost TEXT,
+            proffy_id INTEGER
+        );
+        
+        CREATE TABLE IF NOT EXISTS classes_schedule(
+            id INTEGER PRAMARY KEY AUTOINCREMENT,
+            class_id INTEGER,
+            weekday INTEGER,
+            time_from INTEGER,
+            time_to INTEGER
+        ) 
+    `)
 }
