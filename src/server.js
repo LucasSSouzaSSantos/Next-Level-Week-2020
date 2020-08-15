612b22efb0910438945a9a1602884ca0b4d242e0
Lucas <lucsousan@gmail.com>
2020-08-14 23:39:21 -0300

    fim da penultima aula

--- src/server.js
+++ src/server.js
@@ -1,24 +1,45 @@
-const proffys = {
-  name : "Diego Fernandes",
-  avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
-  whatsapp : "85843823485", 
-  bio : "este é só um teste", 
-  subject: "Química",
-  cost: "20", 
-  weekday : [0], 
-  time_from : [720], 
-  time_to: [1220]
+const proffys = [{
+    name: "Diego Fernandes",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "67584574",
+    bio: "Entusiasta das melhores tecnologias de químicac avançada, apaixonado pro explodir coisas em laboratório e por mudar a vida das pessoas através de experi^encias. Mais 200.000 pessoas já passaramm por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20,00",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220]
+}]
+
+const subjects = [
+  "Artes", "Biologia", "Ciências", "Educação física", "Física", "Geografia", "História", "Matemática",
+  "Português", "Química",
+]
+
+const weekdays = [
+  "Domingo", "Seguda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sábado",
+]
+
+function getSubject(subjectNumber) {
+  const position = +subjectNumber - 1
+  return subjects[position]  
 }
-const title = "Hi friend"
 
 function pageLanding(req, res) {
   return res.render("index.html") 
 }
 function pageStudy(req, res) {
-  return res.render("study.html", {proffys, title})
+  const filters = req.query
+  return res.render("study.html", {proffys, filters, subjects, weekdays})
 }
 function pageGiveClasses(req, res) {
-    return res.render("give-classes.html")
+    const data = req.query
+    const isNotEmpty = Object.keys(data).length > 0
+    if(isNotEmpty){
+      data.subject = getSubject(data.subject)
+      proffys.push(data)
+      return res.redirect("/study")
+    }
+    return res.render("give-classes.html", {subjects, weekdays})
 }
 
 const express = require('express')
