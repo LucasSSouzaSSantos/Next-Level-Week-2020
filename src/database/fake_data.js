b98f8f7226fce323face9f1ea939421a2afc9ac9
Lucas <lucsousan@gmail.com>
2020-08-16 18:33:04 -0300

    novo arquivo

---
+++ src/database/fake_data.js
@@ -0,0 +1,11 @@
+const proffys = [{
+    name: "Diego Fernandes",
+    avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+    whatsapp: "67584574",
+    bio: "Entusiasta das melhores tecnologias de químicac avançada, apaixonado pro explodir coisas em laboratório e por mudar a vida das pessoas através de experi^encias. Mais 200.000 pessoas já passaramm por uma das minhas explosões.",
+    subject: "Química",
+    cost: "20,00",
+    weekday: [0],
+    time_from: [720],
+    time_to: [1220]
+}]
